b852e7979ff5ce1a99443703b7d3b2e14be303fd
Guten <ywzhaifei@gmail.com>
2024-08-13 09:46:11 +0800

    fix(Android): make build working (#9)
    * fix(Android): manifest
    
    * revert credentials version

--- android/src/main/java/expo/modules/passkeys/ReactNativePasskeysPackage.kt
+++ android/src/main/java/expo/modules/passkeys/ReactNativePasskeysPackage.kt
@@ -1,36 +1,16 @@
 package expo.modules.passkeys
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import java.util.HashMap
+import com.facebook.react.uimanager.ViewManager
 
-class ReactNativePasskeysPackage : TurboReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == ReactNativePasskeysModule.NAME) {
-      ReactNativePasskeysModule(reactContext)
-    } else {
-      null
-    }
+class ReactNativePasskeysPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(ReactNativePasskeysModule(reactContext))
   }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      val isTurboModule: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-      moduleInfos[ReactNativePasskeysModule.NAME] =
-          ReactModuleInfo(
-              ReactNativePasskeysModule.NAME,
-              ReactNativePasskeysModule.NAME,
-              false, // canOverrideExistingModule
-              false, // needsEagerInit
-              true, // hasConstants
-              false, // isCxxModule
-              isTurboModule // isTurboModule
-          )
-      moduleInfos
-    }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
   }
 }
